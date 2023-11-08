4f3b662df0f0c46f49d5bf159b52e57c4289c111
lamarreava <lamarreava@urbandaleschools.com>
2023-11-08 11:13:29 -0600

    ahgfajha

--- Graphic Test File.py
+++ Graphic Test File.py
@@ -1,1 +1,10 @@
 import arcade
+arcade.open_window(600, 600, "Where the Title Goes", True)
+arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+arcade.start_render()
+#WHERE DRAWING GOES
+
+
+arcade.finish_render()
+
+arcade.run()
